f52e843946d883fc6c0fcd07c7bffbb199caa40d
Adan Navejas <adannavejas@Adans-Mac-mini.attlocal.net>
2024-10-07 08:55:31 -0500

    updated prompts

--- src/constants.js
+++ src/constants.js
@@ -8,13 +8,13 @@ export const dialogueData = {
     tv: `That's my TV. I've been watching tech youtubers a lot recently like :
      <a href="https://www.youtube.com/@freecodecamp" target="_blank">FreeCodeCamp</a>, <a href="https://www.youtube.com/@DorianDevelops" target="_blank">Dorian Develops</a>,
     <a href="https://www.youtube.com/@joseanmartinez" target="_blank">Josean Martinez</a> and <a href="https://www.youtube.com/@TiffInTech" target="_blank">TiffInTech</a>!`,
-    bed: `This where I sleep. Great ideas comes when I'm lying on my bed. When an idea strikes, I often have to write it down or else I won't be able to sleep because my mental energy is consumed by it.`,
+    bed: `My bed is where the best ideas come to me. Whenever inspiration hits, I have to jot it down right away; if I don’t, my mind keeps racing, and I can’t fall asleep.`,
     resume: `This is my desk and on it is my resume. <a href="https://github.com/adann44/resume/blob/main/Adans%20Resume%2009.27.pdf" target="_blank">Check it out?</a>
     Contact me at adann44@gmail.com if you have any interesting job opportunities!`,
     projects: `Info about this portfolio : It's made with the Kaboom.js library which is a library for making games in JavaScript.
     Text is rendered with HTML/CSS. So the textbox you're currently reading is not rendered within canvas. Learn more about how to use
     Kaboom.js by watching some of JSLegendDev tutorials <a href="https://youtube.com/@jslegenddev" target="_blank">here</a>.`,
-    library: `There are a lot of programming books on my shelves. There is even one in French (I also speak French btw).
+    library: `There are a lot of programming books on my shelves. There is even one in Spanish (I also speak Spanish btw).
     I probably only read one of them. Who else compulsively buys technical books without ever finishing them?`,
     exit: `If you want to exit JSLegendDev's portfolio, just close the tab.`,
   };
