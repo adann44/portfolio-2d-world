e88e8892eaf072a6fe48bbdd4ce3bcda9af1ab39
Adan Navejas <adannavejas@Adans-Mac-mini.attlocal.net>
2024-10-07 08:43:33 -0500

    added in updated resume

--- src/constants.js
+++ src/constants.js
@@ -1,21 +1,19 @@
 export const scaleFactor = 4;
 export const dialogueData = {
-    pc: `This is my PC. I work mostly in JavaScript/TypeScript these days.
-      I've made a couple of games in that language. I also like Golang and Python. Anyway regardless of the language, I just like programming.
-      Here is my <a href="https://github.com/jslegenddev" target="_blank">Github</a>!`,
-    "cs-degree": `This is my CS degree. I hung it on the wall because I'm proud of it. It was a very theoretical degree but I think it gave me a good foundation.`,
+    pc: `This is my PC. I work mostly in JavaScript/Python these days.
+      Here is my <a href="https://github.com/adann44" target="_blank">Github</a>!`,
+    "cs-degree": `This is my full stack software development bootcamp badge of completion. I hung it on the wall because I'm proud of it. <a href="https://www.credly.com/badges/1974c01b-1380-4637-b1d7-eef91fcc60ee/linked_in?t=sfno8p" target="_blank">View my verified achievement from Upright Education - Credly</a>!`,
     "sofa-table": `That's my sofa. I like to relax here and watch YouTube. 
-    I also make game programming tutorials on YouTube. Go sub to <a href="https://youtube.com/@jslegenddev" target="_blank">my channel</a>! (If you like the content)
-    You'll learn how I built this portfolio you're currently playing through!`,
+    Go watch my <a href="https://www.youtube.com/watch?v=sV9CqJNJH58&t=149s" target="_blank">How To Make a Chocoflan</a> video! (If you like the baking content :) )`,
     tv: `That's my TV. I've been watching tech youtubers a lot recently like :
-     <a href="https://www.youtube.com/@ThePrimeTimeagen" target="_blank">Theprimeagen</a>, <a href="https://www.youtube.com/@t3dotgg" target="_blank">Theo - t3.gg</a>,
-    <a href="https://www.youtube.com/@PirateSoftware" target="_blank">PirateSoftware</a> (sometimes) and <a href="https://www.youtube.com/@MelkeyDev" target="_blank">Melkey</a>!`,
+     <a href="https://www.youtube.com/@freecodecamp" target="_blank">FreeCodeCamp</a>, <a href="https://www.youtube.com/@DorianDevelops" target="_blank">Dorian Develops</a>,
+    <a href="https://www.youtube.com/@joseanmartinez" target="_blank">Josean Martinez</a> and <a href="https://www.youtube.com/@TiffInTech" target="_blank">TiffInTech</a>!`,
     bed: `This where I sleep. Great ideas comes when I'm lying on my bed. When an idea strikes, I often have to write it down or else I won't be able to sleep because my mental energy is consumed by it.`,
-    resume: `This is my desk and on it is my resume. <a href="https://github.com/JSLegendDev/Resume/blob/main/JSLegend%20Resume-1.pdf" target="_blank">Check it out?</a>
-    Contact me at jslegend@protonmail.com if you have any interesting job opportunities!`,
+    resume: `This is my desk and on it is my resume. <a href="https://github.com/adann44/resume/blob/main/Adans%20Resume%2009.27.pdf" target="_blank">Check it out?</a>
+    Contact me at adann44@gmail.com if you have any interesting job opportunities!`,
     projects: `Info about this portfolio : It's made with the Kaboom.js library which is a library for making games in JavaScript.
     Text is rendered with HTML/CSS. So the textbox you're currently reading is not rendered within canvas. Learn more about how to use
-    Kaboom.js by watching some of my tutorials <a href="https://youtube.com/@jslegenddev" target="_blank">here</a>.`,
+    Kaboom.js by watching some of JSLegendDev tutorials <a href="https://youtube.com/@jslegenddev" target="_blank">here</a>.`,
     library: `There are a lot of programming books on my shelves. There is even one in French (I also speak French btw).
     I probably only read one of them. Who else compulsively buys technical books without ever finishing them?`,
     exit: `If you want to exit JSLegendDev's portfolio, just close the tab.`,
